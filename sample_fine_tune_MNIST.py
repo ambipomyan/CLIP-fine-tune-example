e30dc1433c79070186e111fbb420b9c40a5bfb50
Kewei Yan <yankw62@gmail.com>
2023-03-30 19:39:36 -0400

    Update code
    - Correct Tensor shape problem in test method
    - Add zeroshot classifier
    - Update accurancy method

--- sample_fine_tune_MNIST.py
+++ sample_fine_tune_MNIST.py
@@ -9,7 +9,7 @@ import numpy as np
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-model_pre, preprocess_pre = clip.load("RN101", device=device)
+model_pre, preprocess_pre = clip.load('RN50', device=device)
 
 # modifying model structure
 model = model_pre
@@ -36,6 +36,8 @@ preprocess = transforms.Compose([
     transforms.Normalize([0.5, 0.5, 0.5],[0.5, 0.5, 0.5])
 ])
 
+classnames = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ]
+templates = ['a photo of the number: "{}".', ]
 
 class MyDataset(Dataset):
     def __init__(self, data_path, preprocess):
@@ -59,7 +61,31 @@ class MyDataset(Dataset):
         return len(self.data)
 
 
-def train(model, device, train_loader, epochs):
+def per_accurancy(probs):
+    m = max(probs)
+    for i, j in enumerate(probs):
+    	if j == m:
+    	    return i
+    
+
+def zeroshot_classifier(classnames, templates):
+    with torch.no_grad():
+        weights = []
+        for classname in classnames:
+            texts = [template.format(classname) for template in templates]
+            texts = clip.tokenize(texts).to(device)
+            text_features = model.encode_text(texts)
+            class_embeddings = text_features / text_features.norm(dim=-1, keepdim=True)
+            class_embedding = class_embeddings.mean(dim=0)
+            class_embedding /= class_embedding.norm()
+            weights.append(class_embedding)
+
+        weights = torch.stack(weights, dim=1).to(device)
+     
+        return weights
+
+
+def train(model, weights, device, train_loader, epochs):
     loss_fn = nn.CrossEntropyLoss()
     optimizer = Adam(model.fc.parameters(), lr=0.001, weight_decay=0.0001)
 
@@ -69,8 +95,9 @@ def train(model, device, train_loader, epochs):
         total_loss = 0.0
         for idx, data in enumerate(train_loader):
             images, texts = data
-            images = images.squeeze(1).to(device)
-            texts = texts.squeeze(1).to(device)
+            
+            images = images.to(device)
+            texts = texts.to(device)
             
             optimizer.zero_grad()
             
@@ -87,20 +114,22 @@ def train(model, device, train_loader, epochs):
             avg_loss = total_loss / len(train_loader)
             
 
-def test(model, device, test_loader, epochs):
+def test(model, weights, device, test_loader, epochs):
     counts = 0
     with torch.no_grad():
         for idx, data in enumerate(test_loader):
             images, texts = data
-            images = images.squeeze(1).to(device)
-            #texts = clip.tokenize(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]).to(device)
-            texts = clip.tokenize(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]).to(device)
             
-            logits_per_image, logits_per_text = model(images, texts)
-            probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+            images = images.squeeze(1).to(device) 
             
-            #print(np.argmin(probs[0]))
-            if np.argmin(probs[0]) > (int(idx/5)-1) and np.argmin(probs[0]) <= int(idx/5):
+            image_features = model.encode_image(images)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            
+            logits = 100 * image_features @ weights
+            probs = logits.softmax(dim=-1).cpu().numpy()
+            
+            print(per_accurancy(probs[0]))
+            if per_accurancy(probs[0]) > (int(idx/5)-1) and per_accurancy(probs[0]) <= int(idx/5):
                 counts += 1
     
     print(counts*2)
@@ -115,8 +144,10 @@ def main():
     test_set = MyDataset("data/MNIST_test_0.txt", preprocess)
     test_loader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=0)
     
-    train(model, device, train_loader, 20)
-    test(model, device, test_loader, 1)
+    weights = zeroshot_classifier(classnames, templates)
+    
+    #train(model, weights, device, train_loader, 20)
+    test(model, weights, device, test_loader, 1)
 
 if __name__ == '__main__':
     main()
